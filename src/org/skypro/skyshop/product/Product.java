7d25756f80a588dc2b80e8854051950beaa80945
Nitro <artemcuker@gmail.com>
2025-04-28 16:17:28 +0200

    0,2

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,19 +1,33 @@
 package org.skypro.skyshop.product;
 
+// Класс для представления продукта
 public class Product {
+
+    // Название продукта
     private final String name;
+
+    // Стоимость продукта
     private final int price;
 
+    // Конструктор для создания продукта с заданным именем и ценой
     public Product(String name, int price) {
         this.name = name;
         this.price = price;
     }
 
+    // Метод для получения названия продукта
     public String getName() {
         return name;
     }
 
+    // Метод для получения цены продукта
     public int getPrice() {
         return price;
+    }
+
+    // Переопределённый метод toString для вывода информации о продукте
+    @Override
+    public String toString() {
+        return "Product{name='" + name + "', price=" + price + '}';
     }
 }
